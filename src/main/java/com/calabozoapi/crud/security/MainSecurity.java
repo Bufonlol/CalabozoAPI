4b1d6067d0683392a1bc878c35653790c8ab207d
Pachi <111835915+Bufonlol@users.noreply.github.com>
2024-04-07 05:44:55 -0600

    AAA

--- src/main/java/com/calabozoapi/crud/security/MainSecurity.java
+++ src/main/java/com/calabozoapi/crud/security/MainSecurity.java
@@ -3,7 +3,6 @@ package com.calabozoapi.crud.security;
 import com.calabozoapi.crud.security.jwt.JwtEntryPoint;
 import com.calabozoapi.crud.security.jwt.JwtTokenFilter;
 import com.calabozoapi.crud.security.service.UserDetailsServiceImpl;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +16,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -61,24 +56,14 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .cors()
-                .and()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*"); // Permitir todas las solicitudes desde cualquier origen
-        config.addAllowedHeader("*"); // Permitir todos los encabezados
-        config.addAllowedMethod("*"); // Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter();
-    }
-
 }
