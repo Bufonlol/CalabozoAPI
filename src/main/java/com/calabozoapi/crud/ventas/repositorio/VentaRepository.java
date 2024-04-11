90ae546307c2a268014bea1b417911743b8b818c
Pachi <111835915+Bufonlol@users.noreply.github.com>
2024-04-11 12:18:34 -0600

    AAA

--- src/main/java/com/calabozoapi/crud/ventas/repositorio/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.calabozoapi.crud.ventas.repositorio;
-
-import com.calabozoapi.crud.ventas.modelo.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
-}
